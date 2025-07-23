2235bf65df0f3bf9201f6e1710c3cdc3f97f8fe5
Samuel Rezende <samuelferreirarezende@gmail.com>
2025-07-23 12:52:19 -0300

    feat: install.sh

---
+++ init.sh
@@ -0,0 +1,341 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Ensure computer doesn't go to sleep or lock while installing
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.session idle-delay 0
+
+echo "Installing terminal and desktop tools..."
+
+# Install terminal tools
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y curl git unzip
+
+## Setup Shell
+sudo apt install zsh -y
+sudo usermod -s $(which zsh) $(whoami)
+
+## Btop
+sudo apt install btop -y
+
+## Fastfetch
+sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+sudo apt update -y
+sudo apt install -y fastfetch
+
+## Github CLI
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+	sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+	sudo apt update &&
+	sudo apt install gh -y
+
+
+## LazyGit
+cd /tmp
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -sLo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar -xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+rm lazygit.tar.gz lazygit
+mkdir -p ~/.config/lazygit/
+touch ~/.config/lazygit/config.yml
+cd -
+
+## Neovim
+cd /tmp
+wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz"
+tar -xf nvim.tar.gz
+sudo install nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+sudo cp -R nvim-linux-x86_64/lib /usr/local/
+sudo cp -R nvim-linux-x86_64/share /usr/local/
+rm -rf nvim-linux-x86_64 nvim.tar.gz
+cd -
+
+### Install luarocks and tree-sitter-cli to resolve lazyvim :checkhealth warnings
+sudo apt install -y luarocks tree-sitter-cli
+
+## Tmux
+sudo apt install tmux -y
+
+## Stow
+sudo apt install stow -y
+
+## Git-crypt
+sudo apt install git-crypt -y
+
+## Terminal Apps
+sudo apt install -y fzf ripgrep bat eza fd-find
+
+## Docker
+### Add the official Docker repo
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo wget -qO /etc/apt/keyrings/docker.asc https://download.docker.com/linux/ubuntu/gpg
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+
+### Install Docker engine and standard plugins
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+
+### Give this user privileged Docker access
+sudo usermod -aG docker ${USER}
+
+### Limit log size to avoid running out of disk
+echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
+
+## Libraries
+sudo apt install -y \
+  build-essential pkg-config autoconf bison clang rustc \
+  libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
+  libvips imagemagick libmagickwand-dev mupdf mupdf-tools gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
+  redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev libpq-dev postgresql-client postgresql-client-common
+
+## Mise
+sudo apt update -y && sudo apt install -y gpg wget curl
+sudo install -dm 755 /etc/apt/keyrings
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=$(dpkg --print-architecture)] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt update
+sudo apt install -y mise
+
+## NodeJS
+mise use --global node@lts
+
+## PHP
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt -y install php8.3 php8.3-{curl,apcu,intl,mbstring,opcache,pgsql,mysql,sqlite3,redis,xml,zip}
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php --quiet && sudo mv composer.phar /usr/local/bin/composer
+rm composer-setup.php
+
+## Redis
+sudo docker run -d --restart unless-stopped -p "6379:6379" --name=redis7 redis:7
+
+## Postgres
+sudo docker run -d --restart unless-stopped -p "5432:5432" --name=postgres17 -e POSTGRES_HOST_AUTH_METHOD=trust postgres:17
+
+## Set Git
+git config --global user.name "Samuel Rezende"
+git config --global user.email "samuelferreirarezende@gmail.com"
+
+# Install desktop tools and tweaks
+
+## Flatpak
+sudo apt install -y flatpak
+sudo apt install -y gnome-software-plugin-flatpak
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+## Kitty
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh
+sudo ln -s /home/$USER/.local/kitty.app/bin/kitty /usr/local/bin/
+cp /home/$USER/.local/kitty.app/share/applications/kitty.desktop /home/$USER/.local/share/applications
+sed -i "s|Icon=Kitty|Icon=/home/$USER/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" /home/$USER/.local/share/applications/kitty.desktop
+
+## Chrome
+cd /tmp
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y ./google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+xdg-settings set default-web-browser google-chrome.desktop
+cd -
+
+## Flameshot
+sudo apt install -y flameshot
+
+## Gnome Sushi
+sudo apt install -y gnome-sushi
+
+## Gnome Tweak
+sudo apt install -y gnome-tweak-tool
+
+## Localsend
+cd /tmp
+LOCALSEND_VERSION=$(curl -s "https://api.github.com/repos/localsend/localsend/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+wget -O localsend.deb "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-x86-64.deb"
+sudo apt install -y ./localsend.deb
+rm localsend.deb
+cd -
+
+## Obsidian
+flatpak install -y flathub md.obsidian.Obsidian
+
+## VLC
+sudo apt install -y vlc
+
+## VSCode
+cd /tmp
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
+rm -f packages.microsoft.gpg
+cd -
+
+sudo apt update -y
+sudo apt install -y code
+
+## WL-clipboard
+sudo apt install wl-clipboard -y
+
+## Fonts
+mkdir -p ~/.local/share/fonts
+
+cd /tmp
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
+unzip JetBrainsMono.zip -d JetBrainsFont
+cp JetBrainsFont/*.ttf ~/.local/share/fonts
+rm -rf JetBrainsMono.zip JetBrainsFont
+
+wget -O iafonts.zip https://github.com/iaolo/iA-Fonts/archive/refs/heads/master.zip
+unzip iafonts.zip -d iaFonts
+cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
+rm -rf iafonts.zip iaFonts
+
+fc-cache
+cd -
+
+## Gnome Extensions
+sudo apt install -y gnome-shell-extension-manager pipx
+pipx ensurepath
+pipx install gnome-extensions-cli --system-site-packages
+
+gnome-extensions disable tiling-assistant@ubuntu.com
+gnome-extensions disable ubuntu-appindicators@ubuntu.com
+gnome-extensions disable ubuntu-dock@ubuntu.com
+gnome-extensions disable ding@rastersoft.com
+
+gext install just-perfection-desktop@just-perfection
+gext install blur-my-shell@aunetx
+gext install space-bar@luchrioh
+gext install dash-to-dock@micxgx.gmail.com
+gext install appindicatorsupport@rgcjonas.gmail.com
+gext install clipboard-indicator@tudmotu.com
+
+sudo cp ~/.local/share/gnome-shell/extensions/just-perfection-desktop\@just-perfection/schemas/org.gnome.shell.extensions.just-perfection.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/blur-my-shell\@aunetx/schemas/org.gnome.shell.extensions.blur-my-shell.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/space-bar\@luchrioh/schemas/org.gnome.shell.extensions.space-bar.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/dash-to-dock\@micxgx.gmail.com/schemas/org.gnome.shell.extensions.dash-to-dock.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/appindicatorsupport\@rgcjonas.gmail.com/schemas/org.gnome.shell.extensions.appindicator.gschema.xml /usr/share/glib-2.0/schemas/
+sudo cp ~/.local/share/gnome-shell/extensions/clipboard-indicator\@tudmotu.com/schemas/org.gnome.shell.extensions.clipboard-indicator.gschema.xml /usr/share/glib-2.0/schemas/
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
+
+### Configure Just Perfection
+gsettings set org.gnome.shell.extensions.just-perfection animation 2
+gsettings set org.gnome.shell.extensions.just-perfection dash-app-running true
+gsettings set org.gnome.shell.extensions.just-perfection workspace true
+gsettings set org.gnome.shell.extensions.just-perfection workspace-popup false
+
+### Configure Space Bar
+gsettings set org.gnome.shell.extensions.space-bar.behavior smart-workspace-names false
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-activate-workspace-shortcuts false
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts enable-move-to-workspace-shortcuts true
+gsettings set org.gnome.shell.extensions.space-bar.shortcuts open-menu "@as []"
+
+## Gnome Hotkeys
+gsettings set org.gnome.desktop.wm.keybindings close "['<Super>q']"
+gsettings set org.gnome.mutter dynamic-workspaces false
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
+
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "[]"
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "[]"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/']"
+
+gsettings set org.gnome.desktop.wm.keybindings switch-input-source "@as []"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Ulauncher'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'ulauncher-toggle'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>space'
+
+gsettings set org.gnome.shell.keybindings screenshot '[]'
+gsettings set org.gnome.shell.keybindings screenshot-window '[]'
+gsettings set org.gnome.shell.keybindings show-screenshot-ui '[]'
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ name 'Flameshot'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ command 'sh -c -- "flameshot gui"'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/ binding 'Print'
+
+## Gnome Settings
+
+### Center new windows in the middle of the screen
+gsettings set org.gnome.mutter center-new-windows true
+
+### Reveal week numbers in the Gnome calendar
+gsettings set org.gnome.desktop.calendar show-weekdate true
+
+### Turn off ambient sensors for setting screen brightness (they rarely work well!)
+gsettings set org.gnome.settings-daemon.plugins.power ambient-enabled false
+
+## ULauncher
+sudo add-apt-repository universe -y && sudo add-apt-repository ppa:agornostal/ulauncher -y && sudo apt update && sudo apt install ulauncher -y
+
+## 1Password
+### Install 1password and 1password-cli single script
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+
+### Add apt repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+sudo tee /etc/apt/sources.list.d/1password.list
+
+### Add the debsig-verify policy
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+
+### Install 1Password & 1password-cli
+sudo apt update && sudo apt install -y 1password 1password-cli
+
+## Spotify
+# Stream music using https://spotify.com
+curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb [signed-by=/etc/apt/trusted.gpg.d/spotify.gpg] http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update -y
+sudo apt install -y spotify-client
+
+## Cursor
+cd /tmp
+mkdir -p /home/$USER/.local/share/sfr/applications/icons
+cp /home/$USER/www/setup-ubuntu/assets/cursor.png /home/$USER/.local/share/sfr/applications/icons/cursor.png
+curl -L "https://www.cursor.com/api/download?platform=linux-x64&releaseTrack=stable" | jq -r '.downloadUrl' | xargs curl -L -o cursor.appimage
+sudo mv cursor.appimage /opt/cursor.appimage
+sudo chmod +x /opt/cursor.appimage
+sudo apt install -y fuse3
+sudo apt install -y libfuse2t64
+
+DESKTOP_FILE="/usr/share/applications/cursor.desktop"
+
+sudo bash -c "cat > $DESKTOP_FILE" <<EOL
+[Desktop Entry]
+Name=Cursor
+Comment=AI-powered code editor
+Exec=/opt/cursor.appimage --no-sandbox
+Icon=/home/$USER/.local/share/sfr/applications/icons/cursor.png
+Type=Application
+Categories=Development;IDE;
+EOL
+
+cd -
+
+# Revert to normal idle and lock settings
+gsettings set org.gnome.desktop.screensaver lock-enabled true
+gsettings set org.gnome.desktop.session idle-delay 300
